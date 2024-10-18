f59185f541486f85bc8f208bc062c7fc7ba43a06
saksham1546 <154740531+saksham1546@users.noreply.github.com>
2024-10-18 21:30:35 +0530

    responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,37 +1,70 @@
-import React, { useContext, useState } from 'react'
-import './Navbar.css'
-import logo from '../Assets/logo.png'
-import cart_icon from '../Assets/cart_icon.png'
-import { Link } from 'react-router-dom'
-import { ShopContext } from '../../Context/ShopContext'
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../Assets/logo.png';
+import { Menu, X, ShoppingCart } from 'lucide-react';
+import { ShopContext } from '../../Context/ShopContext';
+import './Navbar.css';
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { getTotalCartItems } = useContext(ShopContext);
 
-  const [menu,setMenu] = useState("shop");
-  const {getTotalCartItems}=useContext(ShopContext);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const menuItems = [
+    { name: 'shop', path: '/', label: 'Shop' },
+    { name: 'mens', path: '/mens', label: 'Men' },
+    { name: 'womens', path: '/womens', label: 'Women' },
+    { name: 'kids', path: '/kids', label: 'Kids' },
+  ];
 
   return (
-    <div className='navbar'>
+    <nav className="navbar">
+      <div className="nav-left">
         <div className="nav-logo">
-            <img src={logo} alt="" />
-            <p>Zero Fashion</p>
+          <img src={logo} alt="Zero Fashion Logo" className="logo-img" />
+          <p className="desktop-only">Zero Fashion</p>
         </div>
-        <div className='search-bar'>
-          <input type="text" placeholder='Search' />
-        </div>
-        <ul className="nav-menu">
-          <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration:'none'}} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
-          <li onClick={()=>{setMenu("mens")}}><Link style={{textDecoration:'none'}} to='/mens'>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
-          <li onClick={()=>{setMenu("womens")}}><Link style={{textDecoration:'none'}} to='/womens'>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
-          <li onClick={()=>{setMenu("kids")}}><Link style={{textDecoration:'none'}} to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
-        </ul>
-        <div className="nav-login-cart">
-          <Link to='/login'><button>Login</button></Link>
-          <Link to='/CartItems'><img src={cart_icon} alt="" /></Link>
-          <div className="nav-cart-count">{getTotalCartItems()}</div>
-        </div>
-    </div>
-  )
-}
 
-export default Navbar
+        <div className="search-wrapper">
+          <input type="text" placeholder="Search" className="search-input" />
+        </div>
+      </div>
+
+      <ul className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
+        {menuItems.map((item) => (
+          <li key={item.name} onClick={() => {
+            setMenu(item.name);
+            setIsMenuOpen(false);
+          }}>
+            <Link to={item.path}>
+              {item.label}
+            </Link>
+            {menu === item.name && <hr />}
+          </li>
+        ))}
+      </ul>
+
+      <div className="nav-right">
+        <button className="hamburger" onClick={toggleMenu}>
+          {isMenuOpen ? <X size={20} /> : <Menu size={20} />}
+        </button>
+        
+        <div className="nav-login-cart">
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+          <Link to="/cart" className="cart-icon-wrapper">
+            <ShoppingCart size={20} />
+            <span className="nav-cart-count">{getTotalCartItems()}</span>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
